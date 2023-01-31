080ac4a7c682662d2c06202c5f6dd3b8167a9b76
jangg0627 <jangg0627@naver.com>
2023-01-31 10:54:20 +0900

    ADD: react route 설정하기(react-router-dom -> Route, Switch)

--- udemy-react/react-route/src/App.js
+++ udemy-react/react-route/src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPages from "./pages/AllMeetups";
+import FavoritesPages from "./pages/Favorites";
+import NewMeetupsPages from "./pages/NewMeetup";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPages />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPages />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPages />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
