d06eb31b1d89e8edc8815f42b6ac78cb875f83d8
jangg0627 <jangg0627@naver.com>
2023-02-01 14:29:52 +0900

    ADD: useHistory를 사용해 POST가 정상적으로 처리된다면 home 화면으로 이동

--- udemy-react/react-route/src/pages/NewMeetup.js
+++ udemy-react/react-route/src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPages() {
+  const histoty = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-4b283-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
@@ -11,7 +14,9 @@ function NewMeetupsPages() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      histoty.replace("/");
+    });
   }
 
   return (
