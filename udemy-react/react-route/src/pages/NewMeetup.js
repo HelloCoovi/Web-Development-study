a4cf81c3ee6d555f450537c3d85098055709f339
jangg0627 <jangg0627@naver.com>
2023-02-01 14:26:19 +0900

    ADD: fetch로 POST request 요청 보내기

--- udemy-react/react-route/src/pages/NewMeetup.js
+++ udemy-react/react-route/src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPages() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-4b283-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>New Meetups Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
